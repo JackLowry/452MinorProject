8d3067b9d8adb6dde71316f110ddf8df8f86abbd
Irfan Peer <ip233@scarletmail.rutgers.edu>
2021-10-25 15:42:16 +0000

    AddedLoginPageANDAddedRestaurantDropdown---Irfan/Skandan

---
+++ app.py
@@ -0,0 +1,38 @@
+#render_template allows for other files to be called in return statements
+from flask import Flask, render_template #import flask into this file and render_template
+restaurants = ["Applebees", "Olive Garden", "McDonalds", "Popeyes"]
+app = Flask(__name__)
+
+print(__name__) 
+
+@app.route('/') #someone has visited base url and we have to provide information
+def login(): #return some object to be displayed to the user | general python syntax for defining a function
+    return render_template('home.html') #name='Irfan'
+
+@app.route('/about')
+def about(): #name of function and name of route do not have to match
+    return 'This is a url shortener'
+
+@app.route('/Restaurant')
+def dropdown():
+    return render_template('dropdown.html', content = ["Applebees", "Olive Garden", "McDonalds", "Popeyes"])
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+# from flask import Flask, render_template
+# restaurants = ["Applebees", "Olive Garden", "McDonalds", "Popeyes"]
+# app = Flask(__name__)
+
+# # @app.route("/<name>")
+# # def home(name):
+# #     return render_template("index.html", content = name)
+
+# @app.route("/Restaurant")
+# def dropdown():
+#     return render_template("dropdown.html", content = ["Applebees", "Olive Garden", "McDonalds", "Popeyes"])
+
+# if __name__ == "__main__":
+#     app.run()
